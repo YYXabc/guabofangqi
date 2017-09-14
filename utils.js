ae595dc2aa26dc01ee953bef94cacfb2672d5d9a
新泽西的小杰克 <18692271381@sina.cn.com>
2017-09-14 21:56:54 +0800

    9/14

---
+++ utils.js
@@ -0,0 +1,9 @@
+var log = console.log.bind(console)
+var e = function(sel){
+  return document.getElementById(sel);
+}
+
+var path = function(path){
+  path = "D://爬墙music/"+path;
+  return path;
+}
